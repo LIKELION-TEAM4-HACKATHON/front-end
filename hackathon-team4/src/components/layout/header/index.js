f0c330afd293b64b78b6a305fca0525e94695e5c
doyeon <cat0806@naver.com>
2024-07-30 23:22:30 +0900

    fix: 로그아웃 후 메인페이지로 자동 리다이렉트

--- hackathon-team4/src/components/layout/header/index.js
+++ hackathon-team4/src/components/layout/header/index.js
@@ -143,6 +143,7 @@ const Header = () => {
     localStorage.removeItem("refreshToken");
     setIsLoggedIn(false);
     alert("정상적으로 로그아웃 되었습니다.");
+    window.location.href = "/";
   };
 
   return (
