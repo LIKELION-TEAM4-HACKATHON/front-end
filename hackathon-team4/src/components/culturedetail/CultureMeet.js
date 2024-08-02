2af4ff68e3d11d4d367c71f327b176a0b83277d7
doyeon <cat0806@naver.com>
2024-08-02 16:46:40 +0900

    style: 모임 카드 디자인 수정

--- hackathon-team4/src/components/culturedetail/CultureMeet.js
+++ hackathon-team4/src/components/culturedetail/CultureMeet.js
@@ -6,31 +6,38 @@ const CultureMeet = ({ clubs }) => {
     <MeetContainer>
       {clubs.map((club) => (
         <MeetCard key={club.clubId}>
-          <div className="top">
-            <img
-              className="profile-img"
-              src={club.leaderProfileImageUrl}
-              alt="프로필 이미지"
-            />
-            <div className="meet-name">{club.cultureName} 모임</div>
-            <div className="location">{club.regionName}</div>
-          </div>
-          <div className="culture-type">{club.title}</div>
-          <button
-            className="meet-play-btn"
-            onClick={() => {
-              /*여기에 모임링크*/
-            }}
-          >
-            {new Date(club.meetingDate).toLocaleDateString()} 모임
-          </button>
-          <div className="tag-container">
-            <p>
-              #{club.genderRestriction} #{club.ageRestriction}
-            </p>
-            <p>
-              {club.currentParticipant}/{club.maxParticipant}
-            </p>
+          <div className="chat">
+            <div className="popular-chat-content">
+              <div className="chat-title-box">
+                <img
+                  className="profile-img"
+                  src={club.leaderProfileImageUrl}
+                  alt="프로필 이미지"
+                />
+                <div className="chat-title">{club.cultureName} 모임</div>
+              </div>
+              <div className="region">{club.regionName}</div>
+              <div className="category">{club.title}</div>
+              <button
+                className="due-date"
+                onClick={() => {
+                  /*여기에 모임링크*/
+                }}
+              >
+                {new Date(club.meetingDate).toLocaleDateString()} 모임
+              </button>
+              <div className="hashtags-box">
+                <div className="hashtags-left">
+                  <span className="hashtag">#{club.genderRestriction}</span>
+                  <span className="hashtag">#{club.ageRestriction}</span>
+                </div>
+                <div className="hashtags-right">
+                  <span className="limit">
+                    {club.currentParticipant}/{club.maxParticipant}
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
         </MeetCard>
       ))}
@@ -41,10 +48,17 @@ const CultureMeet = ({ clubs }) => {
 const MeetContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  gap: 20px;
+  gap: 30px;
   justify-items: center;
   padding: 20px;
   margin: 20px;
+
+  .chat {
+    margin: 0 200px 84px 0;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(478px, 1fr));
+    gap: 40px;
+  }
 `;
 
 const MeetCard = styled.div`
@@ -54,67 +68,94 @@ const MeetCard = styled.div`
   background: #fff;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset,
     0px 4px 14.1px 0px rgba(0, 0, 0, 0.25);
-  border: none;
-  font-family: KoddiUDOnGothic-Regular;
   padding: 20px;
-  margin: 20px 40px;
+  position: relative;
 
-  .top {
+  .popular-chat-content {
     display: flex;
+    flex-direction: column;
+    height: 100%;
     justify-content: space-between;
-    align-items: center;
-    margin: 12px 10px 5px 10px;
   }
 
-  .profile-img {
-    width: 40px;
-    height: 40px;
+  .chat-title-box {
+    display: flex;
+    align-items: center;
+    img {
+      width: 31px;
+      height: 31px;
+      border-radius: 50%;
+      margin-right: 15px;
+    }
+    .chat-title {
+      color: #000;
+      text-align: center;
+      font-family: KoddiUDOnGothic-Regular;
+      font-size: 18.727px;
+    }
+    margin-bottom: 10px;
   }
-  .meet-name {
-    font-size: 20px;
-    font-weight: 400;
-    color: #000;
-    flex-grow: 1;
-    margin-left: 10px;
-  }
-  .location {
+
+  .region {
     color: #e02525;
-    font-size: 18.727px;
-    font-weight: 400;
+    font-family: KoddiUDOnGothic-Regular;
+    margin-bottom: 4px;
+    position: absolute;
+    right: 20px;
+    top: 20px;
   }
-  .culture-type {
+
+  .category {
     color: #e02525;
-    font-size: 30px;
-    font-weight: 700;
-    margin: 0px 55px;
+    font-family: KoddiUDOnGothic-Bold;
+    font-size: 24.023px;
+    margin-bottom: 4px;
   }
-  .meet-play-btn {
-    width: 372px;
+
+  .due-date {
+    width: auto;
     height: 36px;
     background-color: #cb3939;
-    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset,
-      0px 4px 14.1px 0px rgba(0, 0, 0, 0.25);
-    border-radius: 8px;
     color: #fff;
-    font-size: 18px;
+    text-align: center;
     font-family: KoddiUDOnGothic-Regular;
-    font-weight: 700;
+    font-size: 15px;
+    margin: 16px 0;
+    justify-content: center;
+    align-items: center;
+    display: flex;
     border: none;
-    margin: 10px 55px;
-    cursor: pointer;
   }
 
-  p {
-    color: #7c7c7c;
-    font-size: 18.727px;
-    font-weight: 400;
-    margin: 0px 10px 0px 55px;
-  }
-
-  .tag-container {
+  .hashtags-box {
     display: flex;
     justify-content: space-between;
+    margin-top: 4px;
+  }
+
+  .hashtags-left {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .hashtags-right {
+    display: flex;
     align-items: center;
+  }
+
+  .hashtag {
+    color: #000;
+    font-family: KoddiUDOnGothic-Regular;
+    font-size: 18.727px;
+    margin-right: 10px;
+  }
+
+  .limit {
+    color: #000;
+    font-family: GmarketSans;
+    font-weight: 300;
+    font-style: normal;
+    font-size: 18.727px;
   }
 `;
 
